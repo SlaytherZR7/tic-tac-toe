61f30bb0f83f2655de1b818a3ba44990a8c1cbf9
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-22 22:02:50 -0500

    implement game logic for updating the board, checking for winners, and saving game state

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import { Square } from './components/Square.jsx';
 import { TURNS } from './constants.js';
+import { checkWinnerFrom, checkEndGame } from './logic/board.js';
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js';
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
@@ -12,9 +14,28 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    resetGameStorage();
   };
 
-  const updateBoard = (index) => {};
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn,
+    });
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
+    }
+  };
 
   return (
     <main className='board'>
