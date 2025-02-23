26570fc64caa39d1cc9f1c222461f33bdcb49cc9
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-22 21:58:16 -0500

    implement game state management with reset functionality and board rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
+import { Square } from './components/Square.jsx';
+import { TURNS } from './constants.js';
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const updateBoard = (index) => {};
+
   return (
     <main className='board'>
       <h1 translate='no'>Tic tac toe</h1>
-      <button>Reiniciar juego</button>
-      <section className='game'></section>
+      <button onClick={resetGame}>Reiniciar juego</button>
+      <section className='game'>
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          );
+        })}
+      </section>
       <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
