e27e183a85ca4e09ef82280c7436bed5d035f862
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-22 21:54:25 -0500

    add game board layout and turn display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
-    <main>
-      <h1>Tic tac toe</h1>
+    <main className='board'>
+      <h1 translate='no'>Tic tac toe</h1>
+      <button>Reiniciar juego</button>
+      <section className='game'></section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
     </main>
   );
 }
